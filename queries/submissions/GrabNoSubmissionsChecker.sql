ab65182cf1216e3ac65aec1396624d5f3f35caea
Eric Coan <ecoan@instructure.com>
2016-06-01 08:56:42 -0600

    Fixed a small little typo

--- queries/submissions/GrabNoSubmissionsChecker.sql
+++ queries/submissions/GrabNoSubmissionsChecker.sql
@@ -18,7 +18,7 @@ INNER JOIN course_dim c ON e.course_id = c.id
 INNER JOIN assignment_dim a ON c.id = a.course_id
 LEFT OUTER JOIN submission_fact sf ON a.id = sf.assignment_id AND e.user_id = sf.user_id
 LEFT OUTER JOIN submission_dim sd ON sf.submission_id = sd.id
-INNER JOIN user_dim u ON sf.user_id = u.id
+INNER JOIN user_dim u ON e.user_id = u.id
 WHERE
   (e.type = 'StudentEnrollment' AND e.workflow_state = 'active') AND
   (a.workflow_state = 'published') AND
